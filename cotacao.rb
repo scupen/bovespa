46f07017ecb83abb0709a69a49afd355c3b9ad2b
marcofognog <marcofognog@gmail.com>
2011-04-06 17:24:25 -0300

    works good so far

--- cotacao.rb
+++ cotacao.rb
@@ -5,4 +5,8 @@ class Cotacao < Posifile
 		set_specification("codneg"=>12..23)
 		set_attr_name :codneg
 	end
+	
+	lines_where(0..1,"00") do
+		set_specification("nome_do_arquivo"=>2..14,"codigo_de_origem"=>15..22,"data_de_geracao_do_arquivo"=>23..30)
+	end
 end
--- cotacao_spec.rb
+++ cotacao_spec.rb
@@ -3,8 +3,20 @@ require 'cotacao'
 require 'rspec'
 
 describe Cotacao do
-	it "should be true" do 
+	it "should give information about the header of the file" do
+		cotacao = Cotacao.new("DemoCotacoesHistoricas12022003.txt")
+		cotacao.nome_do_arquivo.should eq("COTAHIST.2003")
+		cotacao.codigo_de_origem.should eq("BOVESPA")
+		cotacao.data_de_geracao_do_arquivo.should eq("20040531")
+	end
+
+	it "should get the code of the asset" do 
 		cotacao = Cotacao.new("DemoCotacoesHistoricas12022003.txt")
 		cotacao.vale5["codneg"].should eq("VALE5")
+	end
+
+	it "should return a list of every asset turned into attribute" do	
+		cotacao = Cotacao.new("DemoCotacoesHistoricas12022003.txt")
+		cotacao.pos_attributes.length.should be(482)
 	end
 end
