967c0373644ad03b1bf8ea38496d7d4125d90ad3
marcofognog <marcofognog@gmail.com>
2011-04-09 18:37:25 -0300

    removing files

--- test_cotacao.rb
+++
@@ -1,9 +0,0 @@
-require 'test/unit'
-require 'cotacao'
-
-class TestCotacao < Test::Unit::TestCase
-	def test_cotacao1
-		cotacao = Cotacao.new("DemoCotacoesHistoricas12022003.txt")
-		assert true
-	end
-end
