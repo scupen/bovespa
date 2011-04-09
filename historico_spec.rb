45a07aae184a1059491a1fb4c0d40042e5c75b84
marcofognog <marcofognog@gmail.com>
2011-04-09 18:38:43 -0300

    changing instance names

--- historico_spec.rb
+++ historico_spec.rb
@@ -4,20 +4,23 @@ require 'rspec'
 
 describe Bovespa::Historico do
 	it "should give information about the header of the file" do
-		cotacao = Bovespa::Historico.new("DemoCotacoesHistoricas12022003.txt")
-		cotacao.nome_do_arquivo.should eq("COTAHIST.2003")
-		cotacao.codigo_de_origem.should eq("BOVESPA")
-		cotacao.data_de_geracao_do_arquivo.should eq("20040531")
+		historico = Bovespa::Historico.new("DemoCotacoesHistoricas12022003.txt")
+		historico.nome_do_arquivo.should eq("COTAHIST.2003")
+		historico.codigo_de_origem.should eq("BOVESPA")
+		historico.data_de_geracao_do_arquivo.should eq("20040531")
 	end
 
 	it "should get the code of the asset" do 
-		cotacao = Bovespa::Historico.new("DemoCotacoesHistoricas12022003.txt")
-		cotacao.vale5["codneg"].should eq("VALE5")
+		historico = Bovespa::Historico.new("DemoCotacoesHistoricas12022003.txt")
+		historico.vale5["codneg"].should eq("VALE5")
 	end
 
 	it "should return a list of every asset turned into attribute" do	
-		cotacao = Bovespa::Historico.new("DemoCotacoesHistoricas12022003.txt")
-			puts cotacao.vale5.inspect
-		cotacao.pos_attributes.length.should be > 478
+		historico = Bovespa::Historico.new("DemoCotacoesHistoricas12022003.txt")
+		historico.pos_attributes.length.should be > 478
+	end
+
+	it "should have valid specified fields" do
+#		cotaca
 	end
 end
